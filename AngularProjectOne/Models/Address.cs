4b9c19d08c29fa681794f22b44d1cc58a630db02
domb777 <50599164+domb777@users.noreply.github.com>
2019-05-15 11:33:10 -0700

    Addition of Address and Customer.cs files (#1)

---
+++ AngularProjectOne/Models/Address.cs
@@ -0,0 +1,105 @@
+﻿//Library access
+using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+[Table(Name = "Address")]
+public class Address
+{
+    //private storage fields      
+    private int customerID;
+    private string country;
+    private string city;
+    private string state;
+    private string street;
+
+	public Address()
+    {
+        country = "";
+        city = "";
+        state = "";
+        street = "";
+    }//Empty Constructor
+
+    //public property access fields
+    //customerID public accessor with primaryKey clause
+    [Column(Storage = "customerID", DbType = "Int NOT NULL IDENTITY", IsPrimaryKey = true, IsDbGenerated = true)]
+    public string CustomerID
+    {
+        get
+        {
+            return this.customerID;
+        }
+        //auto set by database
+    }
+
+    [Column(Storage = "country")]
+    public string Country
+    {
+        get
+        {
+            return this.country;
+        }
+        set
+        {
+            this.country = value;
+        }
+    }
+
+    [Column(Storage = "state")]
+    public string State
+    {
+        get
+        {
+            return this.state;
+        }
+        set
+        {
+            this.state = value;
+        }
+    }
+
+
+    [Column(Storage = "city")]
+    public string City
+    { 
+        get
+        {
+            return this.city;
+        }
+        set
+        {
+            this.city = value;
+        }
+    }
+
+    [Column(Storage = "street")]
+    public string Street
+    {
+        get
+        {
+            return this.street;
+        }
+        set
+        {
+            this.street = value;
+        }
+    }
+    //public Contact getContact(string email, string password)//Query method to get customer information from credentials
+    //{
+    //    DataContext db = new DataContext//sets reference of database context
+    //        (/*INSERT DATABASE REF HERE*/);
+    //
+    //    Table<Contact> Contacts = db.GetTable<Contact>();//gets table from database
+    //    IQueryable <Contacts> emailQuery = //SQL query to get contact associated with email
+    //        from cont in Contact
+    //        where email == Contact.Email
+    //        select cont;
+    //    foreach(Contact c in emailQuery)
+    //    {
+    //        if (c.Customer.Password == password)
+    //            return c;
+    //    }
+    //    return null;
+    //}
+}
---
+++ AngularProjectOne/Models/Customer.cs
@@ -0,0 +1,87 @@
+﻿//Library accesss
+using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+[Table(Name = "Customer")]
+public class Customer
+{
+    //private storage fields
+    private int customerID;
+    private string name;
+    private string gender;
+    private string birthday;
+    private string password;
+
+    public Customer()
+    {
+        name = "";
+        gender = "";
+        birthday = "";
+        password = "";
+    }//Empty Constructor
+
+    //public property access fields
+    //customerID public accessor
+    [Column(Storage = "customerID", DbType = "Int NOT NULL IDENTITY" ,IsPrimaryKey = true, IsDbGenerated = true)]
+    public int CustomerID
+    {
+        get
+        {
+            return this.customerID;
+        }
+    }
+
+    [Column(Storage = "name")]
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    [Column(Storage = "gender")]
+    public string Gender
+    {
+        get
+        {
+            return this.gender;
+        }
+        set
+        {
+            this.gender = value;
+        }
+    }
+
+
+    [Column(Storage = "birthday")]
+    public string Birthday
+    {
+        get
+        {
+            return this.birthday;
+        }
+        set
+        {
+            this.birthday = value;
+        }
+    }
+
+    [Column(Storage = "password")]
+    public string Password
+    {
+        get
+        {
+            return this.password;
+        }
+        set
+        {
+            this.password = value;
+        }
+    }
+}
